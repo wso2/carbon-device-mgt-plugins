81786aee7fb7f37e450371302d99c7d792c8fde0
Dilshan Edirisuriya <ed.dilshan@gmail.com>
2015-05-05 18:10:32 +0530

    New iOS script operation changes

--- features/device-mgt/org.wso2.carbon.device.mgt.mobile.feature/src/main/resources/dbscripts/plugins/h2_ios.sql
+++ features/device-mgt/org.wso2.carbon.device.mgt.mobile.feature/src/main/resources/dbscripts/plugins/h2_ios.sql
@@ -39,14 +39,28 @@ CREATE TABLE IF NOT EXISTS `IOS_FEATURE` (
 
 INSERT INTO IOS_FEATURE (CODE, NAME, DESCRIPTION)
 VALUES
-  ('INSTALL_ENTERPRISE_APPLICATION', 'INSTALL_ENTERPRISE_APPLICATION', 'Enterprise application installation'),
+('INSTALL_ENTERPRISE_APPLICATION', 'INSTALL_ENTERPRISE_APPLICATION', 'Enterprise application installation'),
  ('INSTALL_STORE_APPLICATION', 'INSTALL_STORE_APPLICATION', 'Store application installation'),  
 ('REMOVE_APPLICATION', 'REMOVE_APPLICATION', 'Application removal'),
  ('DEVICE_LOCK', 'DEVICE_LOCK', 'Lock device'),
  ('CELLULAR', 'CELLULAR', 'Cellular operation'),
+ ('APN', 'APN', 'APN operation'),
  ('RESTRICTION', 'RESTRICTION', 'Restriction operation'),
  ('WIFI', 'WIFI', 'WIFI operation'),
  ('DEVICE_INFO', 'DEVICE_INFO', 'Device info operation'),  
 ('AIR_PLAY', 'AIR_PLAY', 'AirPlay operation'),  
 ('LOCATION', 'LOCATION', 'Fetch location'),  
-('ALARM', 'ALARM', 'Alarm device');
+('ALARM', 'ALARM', 'Alarm device'),
+('APPLICATION_LIST', 'APPLICATION_LIST', 'Application list'),
+('PROFILE_LIST', 'PROFILE_LIST', 'Profile list'),
+('REMOVE_PROFILE', 'REMOVE_PROFILE', 'Remove profile'),
+('CLEAR_PASSCODE', 'CLEAR_PASSCODE', 'Clear passcode'),
+('CALDAV', 'CALDAV', 'CalDav'),
+('CALENDAR_SUBSCRIPTION', 'CALENDAR_SUBSCRIPTION', 'Calendar subscription'),
+('PASSCODE_POLICY', 'PASSCODE_POLICY', 'Passcode policy'),
+('EMAIL', 'EMAIL', 'Email operation'),
+('LDAP', 'LDAP', 'LDAP operation'),
+('WEB_CLIP', 'WEB_CLIP', 'Web clip operation'),
+('VPN', 'VPN', 'VPN operation'),
+('PER_APP_VPN', 'PER_APP_VPN', 'Per app VPN operation'),
+('APP_TO_PER_APP_VPN_MAPPING', 'APP_TO_PER_APP_VPN_MAPPING', 'App to per app VPN mapping operation');
