ff7854daaf7ec1a8e440cf36d054522a26689794
milanperera <milanharindu.ucsc@gmail.com>
2015-07-27 10:48:43 +0530

    Merged

--- components/device-mgt/org.wso2.carbon.device.mgt.mobile.impl/src/main/java/org/wso2/carbon/device/mgt/mobile/impl/windows/WindowsDeviceManager.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.mobile.impl/src/main/java/org/wso2/carbon/device/mgt/mobile/impl/windows/WindowsDeviceManager.java
@@ -21,10 +21,7 @@ package org.wso2.carbon.device.mgt.mobile.impl.windows;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.device.mgt.common.*;
-<<<<<<< HEAD
-=======
 import org.wso2.carbon.device.mgt.common.configuration.mgt.ConfigurationEntry;
->>>>>>> d07c226dd52d29fbc7ee2993ecf0f9700aa43ece
 import org.wso2.carbon.device.mgt.common.configuration.mgt.TenantConfiguration;
 import org.wso2.carbon.device.mgt.mobile.dao.MobileDeviceManagementDAOException;
 import org.wso2.carbon.device.mgt.mobile.dao.MobileDeviceManagementDAOFactory;
@@ -49,19 +46,6 @@ public class WindowsDeviceManager implements DeviceManager {
         return null;
     }
 
-<<<<<<< HEAD
-	@Override
-	public boolean saveConfiguration(TenantConfiguration tenantConfiguration) throws DeviceManagementException {
-		return false;
-	}
-
-	@Override
-	public TenantConfiguration getConfiguration() throws DeviceManagementException {
-		return null;
-	}
-
-	@Override
-=======
     @Override
     public boolean saveConfiguration(TenantConfiguration tenantConfiguration)
             throws DeviceManagementException {
@@ -74,7 +58,6 @@ public class WindowsDeviceManager implements DeviceManager {
     }
 
     @Override
->>>>>>> d07c226dd52d29fbc7ee2993ecf0f9700aa43ece
     public boolean modifyEnrollment(Device device) throws DeviceManagementException {
         return true;
     }
