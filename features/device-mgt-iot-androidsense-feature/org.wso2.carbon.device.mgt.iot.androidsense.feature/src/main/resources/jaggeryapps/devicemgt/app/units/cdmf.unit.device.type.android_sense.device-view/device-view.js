17ec1285718b79727abdebdf5a52240c02a68f30
charithag <charitha.ws@gmail.com>
2015-12-15 20:26:21 +0530

    Update device details page

--- features/device-mgt-iot-androidsense-feature/org.wso2.carbon.device.mgt.iot.androidsense.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android_sense.device-view/device-view.js
+++ features/device-mgt-iot-androidsense-feature/org.wso2.carbon.device.mgt.iot.androidsense.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android_sense.device-view/device-view.js
@@ -8,7 +8,6 @@ function onRequest(context) {
         var device = deviceModule.viewDevice(deviceType, deviceId);
 
         if (device && device.status != "error") {
-            log.info(device);
             return {"device": device};
         }
     }
