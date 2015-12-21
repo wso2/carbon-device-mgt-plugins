5e4d7d8cf9da29aca93bc16616b9010aa704049c
charithag <charitha.ws@gmail.com>
2015-12-21 11:36:06 +0530

    Update device operations

--- features/device-mgt-iot-feature/org.wso2.carbon.device.mgt.iot.feature/src/main/resources/jaggeryapps/devicemgt/app/units/iot.unit.device.operation/operation.js
+++
@@ -1,8 +0,0 @@
-function onRequest(context) {
-    var log = new Log("operation.js");
-    var operationModule = require("/app/modules/operation.js").operationModule;
-    var device = context.unit.params.device;
-    log.info(device);
-    var control_operations = operationModule.getControlOperations(device.type);
-    return {"control_operations": control_operations, "device": device};
-}
