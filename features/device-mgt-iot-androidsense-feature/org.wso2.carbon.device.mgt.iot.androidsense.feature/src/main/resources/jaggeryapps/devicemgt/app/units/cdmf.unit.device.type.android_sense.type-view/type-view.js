4285909fc58b242299c5c7b1801ea8fa38b775bd
Menaka Madushanka <menaka12350@gmail.com>
2016-01-08 18:58:17 +0530

    Agent download : corrected the endpoint

--- features/device-mgt-iot-androidsense-feature/org.wso2.carbon.device.mgt.iot.androidsense.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android_sense.type-view/type-view.js
+++ features/device-mgt-iot-androidsense-feature/org.wso2.carbon.device.mgt.iot.androidsense.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.device.type.android_sense.type-view/type-view.js
@@ -2,7 +2,7 @@ function onRequest(context){
     var viewModel = {};
     var process = require("process");
     var serverIP = process.getProperty("carbon.local.ip");
-    var serverPort = process.getProperty("carbon.https.port");
-    viewModel.enrollmentURL = "https://"+serverIP+":"+serverPort+"/android_sense/manager/device/android_sense/download/";
+    var serverPort = process.getProperty("carbon.http.port");
+    viewModel.enrollmentURL = "http://"+serverIP+":"+serverPort+"/android_sense_mgt/manager/device/android_sense/download/";
     return viewModel;
 }
