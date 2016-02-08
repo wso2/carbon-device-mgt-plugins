bcdc2bc764f57609d47a93cb398991f884d23fd5
charithag <charitha.ws@gmail.com>
2016-02-08 10:55:11 +0530

    Move ui and analytics stuffs to components from features

--- features/device-mgt-iot-feature/org.wso2.carbon.device.mgt.iot.feature/src/main/resources/jaggeryapps/devicemgt/app/units/iot.unit.device.stats/stats.js
+++
@@ -1,7 +0,0 @@
-function onRequest(context) {
-    var log = new Log("stats.js");
-    var operationModule = require("/app/modules/operation.js").operationModule;
-    var device = context.unit.params.device;
-    var monitor_operations = JSON.stringify(operationModule.getMonitorOperations(device.type));
-    return {"monitor_operations": monitor_operations, "device": device};
-}
