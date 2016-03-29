f6d04fba4b42de04f8e82f2d9441660fd4ab2746
charithag <charitha.ws@gmail.com>
2016-03-29 12:59:10 +0530

    Fix module locations

--- components/mobile-plugins/mobile-base-plugin/org.wso2.carbon.device.mgt.mobile/src/main/java/org/wso2/carbon/device/mgt/mobile/internal/MobileDeviceManagementDataHolder.java
+++ components/mobile-plugins/mobile-base-plugin/org.wso2.carbon.device.mgt.mobile/src/main/java/org/wso2/carbon/device/mgt/mobile/internal/MobileDeviceManagementDataHolder.java
@@ -19,6 +19,7 @@
 package org.wso2.carbon.device.mgt.mobile.internal;
 
 import org.wso2.carbon.device.mgt.common.spi.DeviceManagementService;
+import org.wso2.carbon.device.mgt.mobile.impl.android.gcm.GCMService;
 import org.wso2.carbon.registry.core.service.RegistryService;
 
 /**
@@ -27,6 +28,8 @@ import org.wso2.carbon.registry.core.service.RegistryService;
 public class MobileDeviceManagementDataHolder {
 
 	private RegistryService registryService;
+	private DeviceManagementService androidDeviceManagementService;
+	private GCMService gcmService;
 
 	private static MobileDeviceManagementDataHolder thisInstance = new MobileDeviceManagementDataHolder();
 
@@ -45,4 +48,20 @@ public class MobileDeviceManagementDataHolder {
 		this.registryService = registryService;
 	}
 
+    public DeviceManagementService getAndroidDeviceManagementService() {
+        return androidDeviceManagementService;
+    }
+
+    public void setAndroidDeviceManagementService(
+            DeviceManagementService androidDeviceManagementService) {
+        this.androidDeviceManagementService = androidDeviceManagementService;
+    }
+
+	public GCMService getGCMService() {
+		return gcmService;
+	}
+
+	public void setGCMService(GCMService gcmService) {
+		this.gcmService = gcmService;
+	}
 }
