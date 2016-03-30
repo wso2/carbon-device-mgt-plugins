8656f84ac6c0cd0ca9d8bb1d4d3a9cd92377da7a
NuwanSameera <nuwansameera74@gmail.com>
2016-03-30 16:14:42 +0530

    Move device type plugins to iot-plugins

---
+++ components/iot-plugins/androidsense-plugin/org.wso2.carbon.device.mgt.iot.androidsense.service.impl/src/main/java/org/wso2/carbon/device/mgt/iot/androidsense/service/impl/transport/AndroidSenseMQTTConnector.java
@@ -0,0 +1,338 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.device.mgt.iot.androidsense.service.impl.transport;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.device.mgt.analytics.data.publisher.exception.DataPublisherConfigurationException;
+import org.wso2.carbon.device.mgt.analytics.data.publisher.service.DeviceAnalyticsService;
+import org.wso2.carbon.device.mgt.common.Device;
+import org.wso2.carbon.device.mgt.common.DeviceIdentifier;
+import org.wso2.carbon.device.mgt.common.DeviceManagementException;
+import org.wso2.carbon.device.mgt.core.service.DeviceManagementProviderService;
+import org.wso2.carbon.device.mgt.iot.androidsense.service.impl.util.DeviceData;
+import org.wso2.carbon.device.mgt.iot.androidsense.service.impl.util.SensorData;
+import org.wso2.carbon.device.mgt.iot.androidsense.plugin.constants.AndroidSenseConstants;
+import org.wso2.carbon.device.mgt.iot.controlqueue.mqtt.MqttConfig;
+import org.wso2.carbon.device.mgt.iot.sensormgt.SensorDataManager;
+import org.wso2.carbon.device.mgt.iot.transport.TransportHandlerException;
+import org.wso2.carbon.device.mgt.iot.transport.mqtt.MQTTTransportHandler;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@SuppressWarnings("no JAX-WS annotation")
+public class AndroidSenseMQTTConnector extends MQTTTransportHandler {
+    private static Log log = LogFactory.getLog(AndroidSenseMQTTConnector.class);
+    private static String subscribeTopic = AndroidSenseConstants.MQTT_SUBSCRIBE_WORDS_TOPIC;
+    private static String iotServerSubscriber = UUID.randomUUID().toString().substring(0, 5);
+
+    private AndroidSenseMQTTConnector() {
+        super(iotServerSubscriber, AndroidSenseConstants.DEVICE_TYPE,
+              MqttConfig.getInstance().getMqttQueueEndpoint(), subscribeTopic);
+    }
+
+    @Override
+    public void connect() {
+        Runnable connector = new Runnable() {
+            public void run() {
+                while (!isConnected()) {
+                    try {
+                        connectToQueue();
+                        subscribeToQueue();
+                    } catch (TransportHandlerException e) {
+                        log.warn("Connection/Subscription to MQTT Broker at: " + mqttBrokerEndPoint + " failed");
+                        try {
+                            Thread.sleep(timeoutInterval);
+                        } catch (InterruptedException ex) {
+                            log.error("MQTT-Subscriber: Thread Sleep Interrupt Exception.", ex);
+                        }
+                    }
+                }
+            }
+        };
+        Thread connectorThread = new Thread(connector);
+        connectorThread.setDaemon(true);
+        connectorThread.start();
+    }
+
+    /**
+     * @throws TransportHandlerException in the event of any exceptions that occur whilst processing the message.
+     */
+    @Override
+    public void processIncomingMessage() throws TransportHandlerException {
+    }
+
+    /**
+     * @param message the message (of the type specific to the protocol) received from the device.
+     * @throws TransportHandlerException
+     */
+    @Override
+    public void processIncomingMessage(MqttMessage message) throws TransportHandlerException {
+    }
+
+
+    @Override
+    public void publishDeviceData(String... publishData) throws TransportHandlerException {
+        if (publishData.length != 3) {
+            String errorMsg = "Incorrect number of arguments received to SEND-MQTT Message. " +
+                    "Need to be [owner, deviceId, content]";
+            log.error(errorMsg);
+            throw new TransportHandlerException(errorMsg);
+        }
+        String deviceId = publishData[0];
+        String operation = publishData[1];
+        String resource = publishData[2];
+        MqttMessage pushMessage = new MqttMessage();
+        String publishTopic = "wso2/" + AndroidSenseConstants.DEVICE_TYPE + "/" + deviceId + "/command";
+        if (operation.equals("add")) {
+            publishTopic = publishTopic + "/words";
+        } else if (operation.equals("remove")) {
+            publishTopic = publishTopic + "/remove";
+        } else if (operation.equals("threshold")) {
+            publishTopic = publishTopic + "/threshold";
+        } else {
+            return;
+        }
+        String actualMessage = resource;
+        pushMessage.setPayload(actualMessage.getBytes(StandardCharsets.UTF_8));
+        pushMessage.setQos(DEFAULT_MQTT_QUALITY_OF_SERVICE);
+        pushMessage.setRetained(false);
+        publishToQueue(publishTopic, pushMessage);
+    }
+
+    @Override
+    public void processIncomingMessage(MqttMessage mqttMessage, String... strings) throws TransportHandlerException {
+        String[] topic = strings[0].split("/");
+        String deviceId = topic[3];
+        if (log.isDebugEnabled()) {
+            log.debug("Received MQTT message for:  & [DEVICE.ID-" + deviceId + "]");
+        }
+        try {
+            Gson gson = new Gson();
+            String actualMessage = mqttMessage.toString();
+            DeviceData deviceData = gson.fromJson(actualMessage, DeviceData.class);
+
+            SensorData[] sensorData = deviceData.values;
+            String streamDef = null;
+            Object payloadData[] = null;
+            String sensorName = null;
+            PrivilegedCarbonContext.startTenantFlow();
+            PrivilegedCarbonContext ctx = PrivilegedCarbonContext.getThreadLocalCarbonContext();
+            ctx.setTenantDomain("carbon.super", true);
+            for (SensorData sensor : sensorData) {
+                if (sensor.key.equals("battery")) {
+                    streamDef = AndroidSenseConstants.BATTERY_STREAM_DEFINITION;
+                    payloadData = new Float[]{Float.parseFloat(sensor.value)};
+                    sensorName = AndroidSenseConstants.SENSOR_BATTERY;
+                } else if (sensor.key.equals("GPS")) {
+                    streamDef = AndroidSenseConstants.GPS_STREAM_DEFINITION;
+                    String gpsValue = sensor.value;
+                    String gpsValuesString[] = gpsValue.split(",");
+                    Float gpsValues[] = new Float[2];
+                    gpsValues[0] = Float.parseFloat(gpsValuesString[0]);
+                    gpsValues[1] = Float.parseFloat(gpsValuesString[0]);
+                    payloadData = gpsValues;
+                    sensorName = AndroidSenseConstants.SENSOR_GPS;
+                } else if (sensor.key.equals("word")) {
+                    try {
+                        streamDef = AndroidSenseConstants.WORD_COUNT_STREAM_DEFINITION;
+                        String[] values = sensor.value.split(",");
+                        String sessionId = values[0];
+                        String keyword = values[1];
+                        int occurrence = Integer.parseInt(values[2]);
+                        String status = values[3];
+                        sensorName = AndroidSenseConstants.SENSOR_WORDCOUNT;
+
+                        if (occurrence > 0) {
+                            payloadData = new Object[]{sessionId, keyword, status};
+                            for (int i = 0; i < occurrence; i++) {
+                                Long timestamp = Long.parseLong(values[3 + occurrence]);
+                                publishDataToDAS(deviceId, timestamp, sensorName, streamDef,
+                                                 sensor.value, payloadData);
+
+                            }
+                            continue;
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        log.error(
+                                "Timestamp does not match the occurence sensor values are sent from the device.");
+                    }
+                    continue;
+                } else {
+                    try {
+                        int androidSensorId = Integer.parseInt(sensor.key);
+                        String sensorValue = sensor.value;
+                        String sensorValuesString[] = sensorValue.split(",");
+                        Float sensorValues[] = new Float[1];
+
+                        switch (androidSensorId) {
+                            case 1:
+                                streamDef = AndroidSenseConstants.ACCELEROMETER_STREAM_DEFINITION;
+                                sensorValues[0] = Float.parseFloat(sensorValuesString[0]) * Float.parseFloat(
+                                        sensorValuesString[0]) * Float.
+                                        parseFloat(sensorValuesString[0]);
+                                payloadData = sensorValues;
+                                sensorName = AndroidSenseConstants.SENSOR_ACCELEROMETER;
+                                break;
+                            case 2:
+                                streamDef = AndroidSenseConstants.MAGNETIC_STREAM_DEFINITION;
+                                sensorValues[0] = Float.parseFloat(sensorValuesString[0]) * Float.parseFloat(
+                                        sensorValuesString[0]) * Float.
+                                        parseFloat(sensorValuesString[0]);
+                                payloadData = sensorValues;
+                                sensorName = AndroidSenseConstants.SENSOR_MAGNETIC;
+                                break;
+                            case 4:
+                                streamDef = AndroidSenseConstants.GYROSCOPE_STREAM_DEFINITION;
+                                sensorValues[0] = Float.parseFloat(sensorValuesString[0]) * Float.parseFloat(
+                                        sensorValuesString[0]) * Float.
+                                        parseFloat(sensorValuesString[0]);
+                                payloadData = sensorValues;
+                                sensorName = AndroidSenseConstants.SENSOR_GYROSCOPE;
+                                break;
+                            case 5:
+                                streamDef = AndroidSenseConstants.LIGHT_STREAM_DEFINITION;
+                                sensorName = AndroidSenseConstants.SENSOR_LIGHT;
+                                payloadData = new Float[]{Float.parseFloat(sensorValuesString[0])};
+                                break;
+                            case 6:
+                                streamDef = AndroidSenseConstants.PRESSURE_STREAM_DEFINITION;
+                                sensorName = AndroidSenseConstants.SENSOR_PRESSURE;
+                                payloadData = new Float[]{Float.parseFloat(sensorValuesString[0])};
+                                break;
+                            case 8:
+                                streamDef = AndroidSenseConstants.PROXIMITY_STREAM_DEFINITION;
+                                sensorName = AndroidSenseConstants.SENSOR_PROXIMITY;
+                                payloadData = new Float[]{Float.parseFloat(sensorValuesString[0])};
+                                break;
+                            case 9:
+                                streamDef = AndroidSenseConstants.GRAVITY_STREAM_DEFINITION;
+                                sensorValues[0] = Float.parseFloat(sensorValuesString[0]) * Float.parseFloat(
+                                        sensorValuesString[0]) * Float.
+                                        parseFloat(sensorValuesString[0]);
+                                payloadData = sensorValues;
+                                sensorName = AndroidSenseConstants.SENSOR_GRAVITY;
+                                break;
+                            case 11:
+                                streamDef = AndroidSenseConstants.ROTATION_STREAM_DEFINITION;
+                                sensorValues[0] = Float.parseFloat(sensorValuesString[0]) * Float.parseFloat(
+                                        sensorValuesString[0]) * Float.
+                                        parseFloat(sensorValuesString[0]);
+                                payloadData = sensorValues;
+                                sensorName = AndroidSenseConstants.SENSOR_ROTATION;
+                                break;
+                        }
+                    } catch (NumberFormatException e) {
+                        log.error("Invalid sensor values are sent from the device.");
+                        continue;
+                    }
+                }
+                publishDataToDAS(deviceId, sensor.time, sensorName, streamDef, sensor.value, payloadData);
+            }
+        } catch (JsonSyntaxException e) {
+            throw new TransportHandlerException("Invalid message format " + mqttMessage.toString());
+        } finally {
+            PrivilegedCarbonContext.endTenantFlow();
+        }
+    }
+
+    private void publishDataToDAS(String deviceId, Long time, String sensorName,
+                                  String streamDefinition, String sensorValue, Object payloadData[]) {
+        try {
+            PrivilegedCarbonContext ctx = PrivilegedCarbonContext.getThreadLocalCarbonContext();
+            DeviceManagementProviderService deviceManagementProviderService = (DeviceManagementProviderService) ctx
+                    .getOSGiService(DeviceManagementProviderService.class, null);
+            if (deviceManagementProviderService != null) {
+                DeviceIdentifier identifier = new DeviceIdentifier(deviceId, AndroidSenseConstants.DEVICE_TYPE);
+                Device device = deviceManagementProviderService.getDevice(identifier);
+                if (device != null) {
+                    String owner = device.getEnrolmentInfo().getOwner();
+                    ctx.setTenantDomain(MultitenantUtils.getTenantDomain(owner), true);
+                    DeviceAnalyticsService deviceAnalyticsService = (DeviceAnalyticsService) ctx
+                            .getOSGiService(DeviceAnalyticsService.class, null);
+                    if (deviceAnalyticsService != null) {
+                        Object metaData[] = {owner, AndroidSenseConstants.DEVICE_TYPE, deviceId, time};
+                        if (streamDefinition != null && payloadData != null && payloadData.length > 0) {
+                            try {
+                                SensorDataManager.getInstance().setSensorRecord(deviceId, sensorName, sensorValue, time);
+                                deviceAnalyticsService.publishEvent(streamDefinition, "1.0.0", metaData,
+                                                                    new Object[0], payloadData);
+                            } catch (DataPublisherConfigurationException e) {
+                                log.error("Data publisher configuration failed - " + e);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (DeviceManagementException e) {
+            log.error("Failed to load device management service.", e);
+        }
+    }
+
+    /**
+     * @throws TransportHandlerException in the event of any exceptions that occur whilst sending the message.
+     */
+    @Override
+    public void publishDeviceData() throws TransportHandlerException {
+
+    }
+
+    /**
+     * @param publishData the message (of the type specific to the protocol) to be sent to the device.
+     * @throws TransportHandlerException in the event of any exceptions that occur whilst sending the message.
+     */
+    @Override
+    public void publishDeviceData(MqttMessage publishData) throws TransportHandlerException {
+
+    }
+
+    @Override
+    public void disconnect () {
+        Runnable stopConnection = new Runnable() {
+            public void run() {
+                while (isConnected()) {
+                    try {
+                        closeConnection();
+                    } catch (MqttException e) {
+                        if (log.isDebugEnabled()) {
+                            log.warn("Unable to 'STOP' MQTT connection at broker at: " + mqttBrokerEndPoint
+                                         + " for device-type - " + AndroidSenseConstants.DEVICE_TYPE, e);
+                        }
+                        try {
+                            Thread.sleep(timeoutInterval);
+                        } catch (InterruptedException e1) {
+                            log.error("MQTT-Terminator: Thread Sleep Interrupt Exception at device-type - " +
+                                          AndroidSenseConstants.DEVICE_TYPE, e1);
+                        }
+                    }
+                }
+            }
+        };
+        Thread terminatorThread = new Thread(stopConnection);
+        terminatorThread.setDaemon(true);
+        terminatorThread.start();
+    }
+}
---
+++ components/iot-plugins/virtual-fire-alarm-plugin/org.wso2.carbon.device.mgt.iot.virtualfirealarm.service.impl/src/main/java/org/wso2/carbon/device/mgt/iot/virtualfirealarm/service/impl/VirtualFireAlarmControllerService.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.device.mgt.iot.virtualfirealarm.service.impl;
+
+import org.wso2.carbon.apimgt.annotations.api.API;
+import org.wso2.carbon.device.mgt.extensions.feature.mgt.annotations.DeviceType;
+import org.wso2.carbon.device.mgt.extensions.feature.mgt.annotations.Feature;
+import org.wso2.carbon.device.mgt.iot.virtualfirealarm.service.impl.dto.DeviceData;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
+
+@API(name = "virtual_firealarm", version = "1.0.0", context = "/virtual_firealarm", tags = "virtual_firealarm")
+@DeviceType(value = "virtual_firealarm")
+public interface VirtualFireAlarmControllerService {
+
+    /**
+     * This is an API used/called by the device. It registers the IP of a VirtualFirealarm device against its DeviceID
+     * when the device connects with the server for the first time. This DeviceID to IP mapping is necessary only for
+     * cases where HTTP communication is to be used. At such instances, this mapping is used by the server to
+     * identify the IP of the device to which it has some message to be sent. This method becomes useful only in
+     * scenarios where HTTP communication is used in a setup where the IoT-Server and the devices communicating with it
+     * are in the same IP network.
+     *
+     * @param deviceId   the ID of the VirtualFirealarm device from which this register-IP call was initiated.
+     * @param deviceIP   the IP of the VirtualFirealarm device which has sent this register-IP request.
+     * @param devicePort the PORT on the VirtualFirealarm device (on this IP) that's open for HTTP communication.
+     * @param request    the HTTP servlet request object received by default as part of the HTTP call to this API.
+     * @return a custom message indicating whether the DeviceID to IP mapping was successful.
+     */
+    @POST
+    @Path("device/register/{deviceId}/{ip}/{port}")
+    Response registerDeviceIP(@PathParam("deviceId") String deviceId, @PathParam("ip") String deviceIP,
+                                   @PathParam("port") String devicePort, @Context HttpServletRequest request);
+
+
+    /**
+     * This is an API called/used from within the Server(Front-End) or by a device Owner. It sends a control command to
+     * the VirtualFirealarm device to switch `ON` or `OFF` its buzzer. The method also takes in the protocol to be used
+     * to connect-to and send the command to the device.
+     *
+     * @param deviceId the ID of the VirtualFirealarm device on which the buzzer needs to switched `ON` or `OFF`.
+     * @param protocol the protocol (HTTP, MQTT, XMPP) to be used to connect-to & send the message to the device.
+     * @param state    the state to which the buzzer on the device needs to be changed. Either "ON" or "OFF".
+     *                 (Case-Insensitive String)
+     */
+    @POST
+    @Path("device/{deviceId}/buzz")
+    @Feature(code = "buzz", name = "Buzzer On / Off", type = "operation",
+            description = "Switch on/off Virtual Fire Alarm Buzzer. (On / Off)")
+    Response switchBuzzer(@PathParam("deviceId") String deviceId, @QueryParam("protocol") String protocol,
+                             @FormParam("state") String state);
+
+    /**
+     * This is an API called/used from within the Server(Front-End) or by a device Owner. It sends a control command
+     * to the VirtualFirealarm device to 'tell what's its current temperature reading'. The method also takes in the
+     * protocol to be used to connect-to and send the command to the device.
+     *
+     * @param deviceId the ID of the VirtualFirealarm device on which the temperature reading is be read-from.
+     * @param protocol the protocol (HTTP, MQTT, XMPP) to be used to connect-to & send the message to the device.
+     * @return an instance of the 'SensorRecord' object that holds the last updated temperature of the VirtualFirealarm
+     * whose temperature reading was requested.
+     */
+    @GET
+    @Path("device/{deviceId}/temperature")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Feature(code = "temperature", name = "Temperature", type = "monitor",
+            description = "Request Temperature reading from Virtual Fire Alarm")
+    Response requestTemperature(@PathParam("deviceId") String deviceId,
+                                           @QueryParam("protocol") String protocol);
+
+    /**
+     * This is an API called/used by the VirtualFirealarm device to publish its temperature to the IoT-Server. The
+     * received data from the device is stored in a 'DeviceRecord' under the device's ID in the 'SensorDataManager'
+     * of the Server.
+     *
+     * @param dataMsg the temperature data received from the device in JSON format complying to type 'DeviceData'.
+     */
+    @POST
+    @Path("device/temperature")
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response pushTemperatureData(final DeviceData dataMsg);
+
+    /**
+     * This is an API called/used by the SCEP Client of the VirtualFirealarm device in its SCEP enrollment process.
+     * This acts as the endpoint exposed as part of the SCEP-Server for use by a SCEP Client. This is one of the two
+     * method-signatures that takes different parameters according to the SCEP-Operation executed by the SCEP-Client
+     * of the enrolling device. The API supports 2 SCEP Operations [GetCACert] and [GetCACaps].
+     * <p/>
+     * Operation [GetCACert] returns the CA cert of the SCEP-Server for the device to verify its authenticity.
+     * Operation [GetCACaps] returns the CA Capabilities of the SCEP-Server.
+     *
+     * @param operation the SCEP operation requested by the client. [GetCACert] or [GetCACaps]
+     * @param message   any messages pertaining to the requested SCEP Operation.
+     * @return an HTTP Response object with either the CA-Cert or the CA-Capabilities according to the operation.
+     */
+    @GET
+    @Path("device/scep")
+    Response scepRequest(@QueryParam("operation") String operation, @QueryParam("message") String message);
+
+
+    /**
+     * This is an API called/used by the SCEP Client of the VirtualFirealarm device in its SCEP enrollment process.
+     * This acts as the endpoint exposed as part of the SCEP-Server for use by a SCEP Client. This is one of the two
+     * method-signatures that takes different parameters according to the SCEP-Operation executed by the SCEP-Client
+     * of the enrolling device. This API supports the SCEP Operation [PKIOperation].
+     * <p/>
+     * Operation [PKIOperation] returns a certificate generated by the SCEP-Server for the enrolling device.
+     *
+     * @param operation   the final SCEP operation executed in the enrollment process - which is [PKIOperation]
+     * @param inputStream an input stream consisting of the Certificate-Signing-Request (CSR) from the device.
+     * @return an HTTP Response object with the signed certificate for the device by the CA of the SCEP Server.
+     */
+    @POST
+    @Path("device/scep")
+    Response scepRequestPost(@QueryParam("operation") String operation, InputStream inputStream);
+
+    /**
+     * Retreive Sensor data for the device type
+     */
+    @Path("device/stats/{deviceId}/sensors/{sensorName}")
+    @GET
+    @Consumes("application/json")
+    @Produces("application/json")
+    Response getVirtualFirealarmStats(@PathParam("deviceId") String deviceId,
+                                                 @PathParam("sensorName") String sensor,
+                                                 @QueryParam("username") String user, @QueryParam("from") long from,
+                                                 @QueryParam("to") long to);
+
+}
