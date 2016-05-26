6773157576a3f36640befa903939042626830e4a
ayyoob <ayyoobhamza@gmail.com>
2016-05-26 17:10:44 +0530

    fixed https://wso2.org/jira/browse/IOTS-177

--- features/iot-plugins-feature/arduino-plugin-feature/org.wso2.carbon.device.mgt.iot.arduino.feature/src/main/resources/dbscripts/oracle.sql
+++ features/iot-plugins-feature/arduino-plugin-feature/org.wso2.carbon.device.mgt.iot.arduino.feature/src/main/resources/dbscripts/oracle.sql
@@ -2,7 +2,7 @@
 -- -----------------------------------------------------
 -- Table for `ARDUINO_DEVICE`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS ARDUINO_DEVICE (
+CREATE  TABLE ARDUINO_DEVICE (
   ARDUINO_DEVICE_ID VARCHAR(45) NOT NULL ,
   DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (ARDUINO_DEVICE_ID) );
--- features/iot-plugins-feature/raspberrypi-plugin-feature/org.wso2.carbon.device.mgt.iot.raspberrypi.feature/src/main/resources/dbscripts/oracle.sql
+++ features/iot-plugins-feature/raspberrypi-plugin-feature/org.wso2.carbon.device.mgt.iot.raspberrypi.feature/src/main/resources/dbscripts/oracle.sql
@@ -2,7 +2,7 @@
 -- -----------------------------------------------------
 -- Table `RASPBERRYPI_DEVICE`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS RASPBERRYPI_DEVICE (
+CREATE  TABLE RASPBERRYPI_DEVICE (
   RASPBERRYPI_DEVICE_ID VARCHAR(45) NOT NULL ,
   DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (RASPBERRYPI_DEVICE_ID) );
