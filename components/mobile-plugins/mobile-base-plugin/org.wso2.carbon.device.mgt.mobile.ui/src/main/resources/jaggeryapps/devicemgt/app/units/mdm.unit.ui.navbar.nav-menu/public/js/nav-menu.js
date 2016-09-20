afea5fcfc6a50921964f86c2b5062a55ed39efea
dilanua <dilan.u.ariyaratne@gmail.com>
2016-09-20 14:21:40 +0530

    Refactoring notifications side-pane - EMM UI

--- components/mobile-plugins/mobile-base-plugin/org.wso2.carbon.device.mgt.mobile.ui/src/main/resources/jaggeryapps/devicemgt/app/units/mdm.unit.ui.navbar.nav-menu/public/js/nav-menu.js
+++ components/mobile-plugins/mobile-base-plugin/org.wso2.carbon.device.mgt.mobile.ui/src/main/resources/jaggeryapps/devicemgt/app/units/mdm.unit.ui.navbar.nav-menu/public/js/nav-menu.js
@@ -131,7 +131,12 @@ function loadNewNotifications() {
                                 // viewModel["appContext"] = context;
                                 $(messageSideBar).html(template(viewModel));
                             } else {
-                                $(messageSideBar).html("<h4 class='text-center'>No new notifications found</h4>");
+                                $(messageSideBar).html(
+                                    "<h4 class='text-center'>No New Notifications</h4>" +
+                                    "<h5 class='text-center text-muted'>" +
+                                        "Check this section for error notifications<br>related to device operations" +
+                                    "</h5>"
+                                );
                             }
                         } else {
                             $(messageSideBar).html("<h4 class ='message-danger text-center'>Unexpected error " +
