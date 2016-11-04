46ad53073526959c1b9215af990e4b23f49ee840
Ace <ruwany.ace@gmail.com>
2016-11-04 19:17:21 +0530

    adding missing files
    continuation from restructuring effort.

---
+++ components/mobile-plugins/windows-plugin/org.wso2.carbon.device.mgt.mobile.windows/src/main/java/org/wso2/carbon/device/mgt/mobile/windows/impl/util/WindowsPluginConstants.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.device.mgt.mobile.windows.impl.util;
+
+/**
+ * Define constance used by Windows plugin.
+ */
+public class WindowsPluginConstants {
+
+    //properties related to database table WINDOWS_DEVICE
+    public static final String DEVICE_ID = "DEVICE_ID";
+    public static final String CHANNEL_URI = "CHANNEL_URI";
+    public static final String DEVICE_INFO = "DEVICE_INFO";
+    public static final String IMEI = "IMEI";
+    public static final String IMSI = "IMSI";
+    public static final String OS_VERSION = "OS_VERSION";
+    public static final String DEVICE_MODEL = "DEVICE_MODEL";
+    public static final String VENDOR = "VENDOR";
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+    public static final String SERIAL = "SERIAL";
+    public static final String MAC_ADDRESS = "MAC_ADDRESS";
+    public static final String DEVICE_NAME = "DEVICE_NAME";
+
+    //Properties related to WIN_FEATURE table
+    public static final String WINDOWS_FEATURE_ID = "ID";
+    public static final String WINDOWS_FEATURE_CODE = "CODE";
+    public static final String WINDOWS_FEATURE_NAME = "NAME";
+    public static final String WINDOWS_FEATURE_DESCRIPTION = "DESCRIPTION";
+
+    public static final String MOBILE_DB_SCRIPTS_FOLDER = "cdm";
+    public static final String MOBILE_CONFIG_REGISTRY_ROOT = "/_system/config";
+
+    public static final String MEDIA_TYPE_XML = "application/xml";
+    public static final String CHARSET_UTF8 = "UTF8";
+    public static final String LANGUAGE_CODE_ENGLISH_US = "en_US";
+    public static final String LANGUAGE_CODE_ENGLISH_UK = "en_UK";
+
+
+}
