ab418dbde37f4569963ac86cf7d718d7e6ed4843
Gobinath <gobinath@wso2.com>
2016-11-30 14:55:53 +0530

    Add new sensors to the android sense plugin
        Incoming and outgoing calls tracker
        Screen on off detector
        SMS tracker
        Audio playing and headset tracker
        Application install and uninstall detector
        User activity detector

--- components/device-types/androidsense-plugin/org.wso2.carbon.device.mgt.iot.androidsense.agent/app/src/main/java/org/wso2/carbon/iot/android/sense/data/publisher/DataPublisherService.java
+++ components/device-types/androidsense-plugin/org.wso2.carbon.device.mgt.iot.androidsense.agent/app/src/main/java/org/wso2/carbon/iot/android/sense/data/publisher/DataPublisherService.java
@@ -25,20 +25,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.wso2.carbon.iot.android.sense.beacon.BeaconScanedData;
+import org.wso2.carbon.iot.android.sense.constants.SenseConstants;
 import org.wso2.carbon.iot.android.sense.data.publisher.mqtt.AndroidSenseMQTTHandler;
 import org.wso2.carbon.iot.android.sense.data.publisher.mqtt.transport.MQTTTransportHandler;
 import org.wso2.carbon.iot.android.sense.data.publisher.mqtt.transport.TransportHandlerException;
-import org.wso2.carbon.iot.android.sense.constants.SenseConstants;
 import org.wso2.carbon.iot.android.sense.event.streams.Location.LocationData;
-import org.wso2.carbon.iot.android.sense.event.streams.Location.LocationDataReader;
-import org.wso2.carbon.iot.android.sense.event.streams.Speed.SpeedData;
 import org.wso2.carbon.iot.android.sense.event.streams.Sensor.SensorData;
+import org.wso2.carbon.iot.android.sense.event.streams.Speed.SpeedData;
+import org.wso2.carbon.iot.android.sense.event.streams.activity.ActivityData;
+import org.wso2.carbon.iot.android.sense.event.streams.application.ApplicationData;
+import org.wso2.carbon.iot.android.sense.event.streams.audio.AudioData;
 import org.wso2.carbon.iot.android.sense.event.streams.battery.BatteryData;
+import org.wso2.carbon.iot.android.sense.event.streams.call.CallData;
+import org.wso2.carbon.iot.android.sense.event.streams.screen.ScreenData;
+import org.wso2.carbon.iot.android.sense.event.streams.sms.SmsData;
 import org.wso2.carbon.iot.android.sense.speech.detector.util.ProcessWords;
 import org.wso2.carbon.iot.android.sense.speech.detector.util.WordData;
-import org.wso2.carbon.iot.android.sense.util.SenseDataHolder;
 import org.wso2.carbon.iot.android.sense.util.LocalRegistry;
-//import org.wso2.carbon.iot.android.sense.util.SenseClient;
+import org.wso2.carbon.iot.android.sense.util.SenseDataHolder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -111,17 +115,21 @@ public class DataPublisherService extends Service {
                     }
                     SenseDataHolder.resetSensorDataHolder();
 
-                    //retrieve batter data.
+                    //retrieve battery data.
                     List<BatteryData> batteryDataMap = SenseDataHolder.getBatteryDataHolder();
                     if (!batteryDataMap.isEmpty()) {
                         for (BatteryData batteryData : batteryDataMap) {
                             Event event = new Event();
                             event.setTimestamp(batteryData.getTimestamp());
                             event.setBattery(batteryData.getLevel());
+                            event.setBatteryTemperature(batteryData.getTemperature());
+                            event.setBatteryStatus(batteryData.getStatus());
+                            event.setBatteryState(batteryData.getState().toString());
                             events.add(event);
                         }
                     }
                     SenseDataHolder.resetBatteryDataHolder();
+
                     //retrieve location data.
                     List<LocationData> locationDataMap = SenseDataHolder.getLocationDataHolder();
 
@@ -132,7 +140,7 @@ public class DataPublisherService extends Service {
                             event.setGps(new double[]{locationData.getLatitude(), locationData.getLongitude()});
                             events.add(event);
                         }
-                     }
+                    }
                     SenseDataHolder.resetLocationDataHolder();
 
                     //retrieve speed data.
@@ -186,6 +194,90 @@ public class DataPublisherService extends Service {
                         }
                     }
                     SenseDataHolder.resetWordDataHolder();
+
+                    // retrieve call data.
+                    List<CallData> callDataList = SenseDataHolder.getCallDataHolder();
+                    if (!callDataList.isEmpty()) {
+                        for (CallData callData : callDataList) {
+                            Event event = new Event();
+                            event.setCallNumber(callData.getPhoneNumber());
+                            event.setCallType(callData.getType().toString().toLowerCase());
+                            event.setCallStartTime(callData.getStartTime());
+                            event.setCallEndTime(callData.getEndTime());
+                            event.setTimestamp(callData.getStartTime());
+
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetCallDataHolder();
+
+                    // retrieve screen data.
+                    List<ScreenData> screenDataList = SenseDataHolder.getScreenDataHolder();
+                    if (!screenDataList.isEmpty()) {
+                        for (ScreenData screenData : screenDataList) {
+                            Event event = new Event();
+                            event.setScreenState(screenData.getAction());
+                            event.setTimestamp(screenData.getTimestamp());
+
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetScreenDataHolder();
+
+                    // retrieve audio data.
+                    List<AudioData> audioDataList = SenseDataHolder.getAudioDataHolder();
+                    if (!audioDataList.isEmpty()) {
+                        for (AudioData audioData : audioDataList) {
+                            Event event = new Event();
+                            event.setTimestamp(audioData.getTimestamp());
+                            event.setAudioPlaying(audioData.isPlaying());
+                            event.setHeadsetOn(audioData.isHeadsetOn());
+                            event.setMusicVolume(audioData.getMusicVolume());
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetAudioDataHolder();
+
+                    // retrieve activity data.
+                    List<ActivityData> activityDataList = SenseDataHolder.getActivityDataHolder();
+                    if (!activityDataList.isEmpty()) {
+                        for (ActivityData activityData : activityDataList) {
+                            Event event = new Event();
+                            event.setTimestamp(activityData.getTimestamp());
+                            event.setActivityType(activityData.getActivity());
+                            event.setConfidence(activityData.getConfidence());
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetActivityDataHolder();
+
+                    // retrieve sms data.
+                    List<SmsData> smsDataList = SenseDataHolder.getSmsDataHolder();
+                    if (!smsDataList.isEmpty()) {
+                        for (SmsData smsData : smsDataList) {
+                            Event event = new Event();
+                            event.setTimestamp(smsData.getTimestamp());
+                            event.setSmsNumber(smsData.getPhoneNumber());
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetSmsDataHolder();
+
+
+                    // retrieve application data.
+                    List<ApplicationData> appDataList = SenseDataHolder.getApplicationDataHolder();
+                    if (!appDataList.isEmpty()) {
+                        for (ApplicationData appData : appDataList) {
+                            Event event = new Event();
+                            event.setTimestamp(appData.getTimestamp());
+                            event.setPackageName(appData.getPackageName());
+                            event.setAction(appData.getAction().toString());
+                            events.add(event);
+                        }
+                    }
+                    SenseDataHolder.resetApplicationDataHolder();
+
+
                     //publish the data
                     if (events.size() > 0 && LocalRegistry.isEnrolled(context)) {
                         String user = LocalRegistry.getUsername(context);
