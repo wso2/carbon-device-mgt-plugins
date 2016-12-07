c15f25c31127b594ccb84d7be3235730ea68a6c4
Harshan Liyanage <harshan@wso2.com>
2016-12-07 14:27:21 +0530

    adding a fix for web app user retrival

--- components/extensions/appm-connector/org.wso2.carbon.appmgt.mdm.osgiconnector/src/main/java/org/wso2/carbon/appmgt/mdm/osgiconnector/mdmmgt/beans/ios/RemoveApplication.java
+++ components/extensions/appm-connector/org.wso2.carbon.appmgt.mdm.osgiconnector/src/main/java/org/wso2/carbon/appmgt/mdm/osgiconnector/mdmmgt/beans/ios/RemoveApplication.java
@@ -23,6 +23,15 @@ import java.io.Serializable;
 public class RemoveApplication implements Serializable {
 
 	private String bundleId;
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
 	public String getBundleId() {
 		return bundleId;
