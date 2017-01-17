c1de144bcf0fe0e52c2281d54396e64fbe2606d7
Dunith Dhanushka <dunithd@gmail.com>
2017-01-17 11:04:19 +0530

    Setting device id and type to page title

--- components/analytics/iot-analytics/org.wso2.carbon.iot.geo.dashboard/src/main/resources/carbonapps/GadgetGeoDashboard_1.0.0/geo-dashboard/js/app.js
+++ components/analytics/iot-analytics/org.wso2.carbon.iot.geo.dashboard/src/main/resources/carbonapps/GadgetGeoDashboard_1.0.0/geo-dashboard/js/app.js
@@ -41,10 +41,24 @@ function initialLoad() {
         processAfterInitializationMap();
         //Access gps and make zoom to server location as map center
         //navigator.geolocation.getCurrentPosition(success, error);
+        setPageTitle();
         $("#loading").hide();
     }
 }
 
+function setPageTitle() {
+  var hash = window.parent.location.hash;
+  if(hash) {
+      var startIdx = hash.indexOf("/") + 1;
+      var lastIdx = hash.length;
+      var deviceInfoString = hash.substring(startIdx,lastIdx);
+      var deviceInfo = JSON.parse(deviceInfoString);
+      if(deviceInfo) {
+        var newTitle = " Geo Dashboard - Device ID: " + deviceInfo.device.id + " Type: " + deviceInfo.device.type;
+        window.parent.document.title =  newTitle;
+      }
+  }
+}
 
 //function success(position) {
 //    var browserLatitude = position.coords.latitude;
