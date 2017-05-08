728a8f4953ab156b678ea30db16f5f32b691a35f
ayyoob <ayyoobhamza@gmail.com>
2017-05-09 00:58:11 +0530

    removed unused files and refactored few device type profile specific  from the new device type feature structure,

--- features/device-types-feature/androidsense-plugin-feature/org.wso2.carbon.device.mgt.iot.androidsense.backend.feature/src/main/resources/dbscripts/oracle.sql
+++
@@ -1,8 +0,0 @@
-
--- -----------------------------------------------------
--- Table `ANDROID_DEVICE`
--- -----------------------------------------------------
-CREATE  TABLE ANDROID_SENSE_DEVICE (
-  ANDROID_DEVICE_ID VARCHAR(45) NOT NULL ,
-  DEVICE_NAME VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (ANDROID_DEVICE_ID) );
