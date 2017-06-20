414af3339e449b4d94a80e24bad0ffb19745e032
Nirothipan <nirothipan@gmail.com>
2017-06-20 20:29:40 +0530

    Givins as system properties

--- components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/android-tryit/startEmulator.bat
+++ components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/android-tryit/startEmulator.bat
@@ -1,19 +1,17 @@
 @echo off
 echo Welcome
 
-SET sdk_tools_url="https://dl.google.com/android/repository/tools_r25.2.5-windows.zip"
-SET platform_tools_url="http://dl.google.com/android/repository/platform-tools_r25.0.3-windows.zip"
-SET build_tools_url="https://dl.google.com/android/repository/build-tools_r25.0.2-windows.zip"
-SET platform_url="https://dl.google.com/android/repository/platform-23_r03.zip"
-SET sys_img_url="https://dl.google.com/android/repository/sys-img/android/x86-23_r09.zip"
-SET haxm_url="https://dl.google.com/android/repository/extras/intel/haxm-windows_r6_0_5.zip"
-
 java^
- -Dsdk_tools_url=%sdk_tools_url%^
- -Dplatform_tools_url=%platform_tools_url%^
- -Dbuild_tools_url=%build_tools_url%^
- -Dplatform_url=%platform_url%^
- -Dsys_img_url=%sys_img_url%^
- -Dhaxm_url=%haxm_url%^
- -jar EmulatorJava.jar
+ -Dsdk.tools.url="https://dl.google.com/android/repository/tools_r25.2.5-windows.zip"^
+  -Dplatform.tools.url="http://dl.google.com/android/repository/platform-tools_r25.0.3-windows.zip"^
+  -Dbuild.tools.url="https://dl.google.com/android/repository/build-tools_r25.0.2-windows.zip"^
+  -Dplatform.url="https://dl.google.com/android/repository/platform-23_r03.zip"^
+  -Dsys.img.url="https://dl.google.com/android/repository/sys-img/android/x86-23_r09.zip"^
+  -Dhaxm.url="https://dl.google.com/android/repository/extras/intel/haxm-windows_r6_0_5.zip"^
+  -Ddownloaded.build.tool.name="android-7.1.1"^
+  -Dbuild.tool.version="25.0.2"^
+  -Ddownloaded.platform.name="android-6.0"^
+  -Dtarget.version="android-23"^
+  -Dos.target="x86"^
+  -jar EmulatorJava.jar
 echo
--- components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/android-tryit/startEmulator.sh
+++ components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.ui/src/main/resources/android-tryit/startEmulator.sh
@@ -6,19 +6,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     OS_SUFFIX="macosx"
 fi
 
-sdk_tools_url="https://dl.google.com/android/repository/tools_r25.2.5-$OS_SUFFIX.zip"
-platform_tools_url="http://dl.google.com/android/repository/platform-tools_r25.0.3-$OS_SUFFIX.zip"
-build_tools_url="https://dl.google.com/android/repository/build-tools_r25.0.2-$OS_SUFFIX.zip"
-platform_url="https://dl.google.com/android/repository/platform-23_r03.zip"
-sys_img_url="https://dl.google.com/android/repository/sys-img/android/x86-23_r09.zip"
-haxm_url="https://dl.google.com/android/repository/extras/intel/haxm-macosx_r6_0_5.zip"
-
 java\
- -Dsdk_tools_url=$sdk_tools_url\
- -Dplatform_tools_url=$platform_tools_url\
- -Dbuild_tools_url=$build_tools_url\
- -Dplatform_url=$platform_url\
- -Dsys_img_url=$sys_img_url\
- -Dhaxm_url=$haxm_url\
+ -Dsdk.tools.url="https://dl.google.com/android/repository/tools_r25.2.5-$OS_SUFFIX.zip"\
+ -Dplatform.tools.url="http://dl.google.com/android/repository/platform-tools_r25.0.3-$OS_SUFFIX.zip"\
+ -Dbuild.tools.url="https://dl.google.com/android/repository/build-tools_r25.0.2-$OS_SUFFIX.zip"\
+ -Dplatform.url="https://dl.google.com/android/repository/platform-23_r03.zip"\
+ -Dsys.img.url="https://dl.google.com/android/repository/sys-img/android/x86-23_r09.zip"\
+ -Dhaxm.url="https://dl.google.com/android/repository/extras/intel/haxm-macosx_r6_0_5.zip"\
+ -Ddownloaded.build.tool.name="android-7.1.1"\
+ -Dbuild.tool.version="25.0.2"\
+ -Ddownloaded.platform.name="android-6.0"\
+ -Dtarget.version="android-23"\
+ -Dos.target="x86"\
  -jar EmulatorJava.jar
 echo
