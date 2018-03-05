82213166d28b8373adee0b21dec4110136611b81
lasantha <tcdlpds@gmail.com>
2018-03-05 12:17:58 +0530

    Fix issues in android-device-mgt-v0.9 copyright text

--- components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.v09.api/src/main/java/org/wso2/carbon/mdm/services/android/bean/wrapper/FileTransferBeanWrapper.java
+++ components/mobile-plugins/android-plugin/org.wso2.carbon.device.mgt.mobile.android.v09.api/src/main/java/org/wso2/carbon/mdm/services/android/bean/wrapper/FileTransferBeanWrapper.java
@@ -15,6 +15,7 @@
 * specific language governing permissions and limitations
 * under the License.
 */
+
 package org.wso2.carbon.mdm.services.android.bean.wrapper;
 
 import io.swagger.annotations.ApiModel;
